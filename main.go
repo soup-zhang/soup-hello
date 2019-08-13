a70dc8ee61997c48a581a4f8a048fccd5f58ab53
soup-zhang <soup-zhang@qq.com>
2019-08-13 10:46:46 +0800

    test

---
+++ main.go
@@ -0,0 +1,23 @@
+/**
+ * @Time : 2019-08-13 10:41 
+ * @Author : soupzhb@gmail.com
+ * @File : main.go
+ * @Software: GoLand
+ */
+
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("main start ...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Soup Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
